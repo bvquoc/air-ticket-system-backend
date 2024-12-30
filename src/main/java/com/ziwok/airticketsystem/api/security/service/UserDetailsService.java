39f3c1093e173a311fbf6bfb6c1f3a9be07dee15
bvquoc <bvquoc.dev@gmail.com>
2024-12-30 12:35:02 +0700

    update src

--- src/main/java/com/ziwok/airticketsystem/api/security/service/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.ziwok.airticketsystem.api.security.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails;
-
-public interface UserDetailsService {
-    UserDetails loadUserByEmail(String username) throws UserDetailsNotFoundException;
-
-}
